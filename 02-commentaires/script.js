8d3567133f7f256346bee8b4ba46bb0e6d094678
Arnaud BODEL <contact@osw3.net>
2020-07-22 15:38:29 +0200

    -

---
+++ 02-commentaires/script.js
@@ -0,0 +1,19 @@
+// Ceci est un commentaire
+
+/* 
+commentaire
+multi 
+lignes
+*/
+
+/**
+ * Commentaire de documentation
+ * 
+ * Return the string "Hello $name"
+ * @param string name 
+ * @return string
+ */
+function sayHello(name)
+{
+    return "Hello "+name;
+}
---
+++ 03-sorties/script.js
@@ -0,0 +1,37 @@
+// Les sorties JS
+
+
+// Sortie en boite de dialogue
+// --
+
+// Boite de dialogue de type ALERT
+// Utilisée pour afficher un  message en boite de dialogue
+alert("Hello World");
+
+// Boite de dialogue de type PROMPT
+// Utilisée pour demander une information à l'utilisateur
+prompt("Write your name");
+
+// Boite de dialogue de type CONFIRM
+// Utilisée pour demander une confirmation à l'utilisateur
+confirm("continue ?");
+
+
+// Sorties en Console
+// --
+
+// Sortie dans la console : Type LOG
+// Utilisée pour les messages d'èvènements et débug
+console.log("Hello World");
+
+// Sortie dans la console : Type INFO
+// Utilisée pour les messages d'info
+console.info("Hello World");
+
+// Sortie dans la console : Type WARNING
+// Utilisée pour les messages d'erreur simple
+console.warn("Hello World");
+
+// Sortie dans la console : Type DANGER
+// Utilisée pour les messages d'erreur fatale
+console.danger("GoodBye World");
