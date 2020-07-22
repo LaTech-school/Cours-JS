9b5bdfbfbb60c32f61fee6057dc00d400e5192a5
Arnaud BODEL <contact@osw3.net>
2020-07-22 16:04:23 +0200

    -

---
+++ 04-variables/script.js
@@ -0,0 +1,26 @@
+// Les variables
+
+// -> Un espace mémoire (dans la RAM)
+// -> prend la taille de ce qu'il contient
+// -> sera de type de ce qu'il contient
+
+// On déclare une variable avec le mot clé "var"
+// Suivi d'un nom de variable (ex: la variable "name")
+var name;
+
+// Affectation d'une valeur, utilisation de l'opérateur d'affection "simple egal"
+// (ex: name prend la valeur de "Bob")
+var name = "Bob";
+
+// Les variables déclarées par "var" ont une portée globale à l'application
+
+// Les variables déclarées par "let" ont une portée locale dans l'application
+
+let age = 2;
+
+
+// Voir le contenu / Appel de la variable
+// --
+
+alert(name);
+console.log(name);
