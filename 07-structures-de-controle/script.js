31040c9a0ee0115f156f4399085746d7f838f923
Arnaud BODEL <contact@osw3.net>
2020-07-23 12:54:59 +0200

    -

---
+++ 07-structures-de-controle/script.js
@@ -0,0 +1,22 @@
+// Structures de contôle
+
+// Condition "if" (si...)
+// --
+// Syntaxe:     if (a) { ... }
+
+var a = 3;
+
+// if (a > 5) {
+//     alert('Variable "a" supérieure a 5');
+// }
+// if (a > 15) {
+//     alert('Variable "a" supérieure a 15');
+// }
+
+// if (false) {
+//     alert("Vraie !");
+// }
+
+
+// Afficher un message si a est supérieur ou égale à 10
+// Afficher un autre message si a est inférieur à 10
