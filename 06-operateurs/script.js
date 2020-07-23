2c936f4c15d0db73cd06dd83ccde5f72377001ae
Arnaud BODEL <contact@osw3.net>
2020-07-23 12:36:19 +0200

    -

--- 06-operateurs/script.js
+++ 06-operateurs/script.js
@@ -8,6 +8,73 @@
 // Syntaxe:     a = b 
 var a = 42;
 
+// Affectation après addition
+// Symbole:     +=
+// Syntaxe:     a += b 
+// Equivalent:  a = a + b
+var a = 10;
+    a += 5;
+console.log('a += 5', a);
+
+// Affectation après soustraction
+// Symbole:     -=
+// Syntaxe:     a -= b 
+// Equivalent:  a = a - b
+var a = 10;
+a -= 5;
+console.log('a -= 5', a);
+
+// Affectation après multiplication
+// Symbole:     *=
+// Syntaxe:     a *= b 
+// Equivalent:  a = a * b
+var a = 10;
+a *= 5;
+console.log('a *= 5', a);
+
+// Affectation après division
+// Symbole:     /=
+// Syntaxe:     a /= b 
+// Equivalent:  a = a / b
+var a = 10;
+a /= 5;
+console.log('a /= 5', a);
+
+// Affectation après Modulo
+// Symbole:     %=
+// Syntaxe:     a %= b 
+// Equivalent:  a = a % b
+var a = 10;
+a %= 4;
+console.log('a %= 4', a);
+
+// Affectation après exponentiation
+// Symbole:     **=
+// Syntaxe:     a **= b 
+// Equivalent:  a = a ** b
+var a = 10;
+a **= 4;
+console.log('a **= 4', a);
+
+// Affectation après décalage à gauche
+// Symbole:     <<=
+// Syntaxe:     a <<= b 
+// Equivalent:  a = a << b
+var a = 10;
+a <<= 4;
+console.log('a <<= 4', a);
+
+// Affectation après décalage à droite
+// Symbole:     >>=
+// Syntaxe:     a >>= b 
+// Equivalent:  a = a >> b
+var a = 10;
+a >>= 4;
+console.log('a >>= 4', a);
+
+
+
+
 
 
 // Les opérateurs de comparaison
@@ -71,6 +138,8 @@ console.log( '84 <= 42', 84 <= 42 );
 console.log( '42 <= "42"', 42 <= "42" );
 console.log( '84 <= "42"', 84 <= "42" );
 console.log( '21 <= "42"', 21 <= "42" );
+
+
 
 
 // Opérateurs arithmétiques
@@ -148,3 +217,24 @@ console.log( 'b3', b3 );
 // Déclarer la variable a = 100
 // Afficher 100 et faire l'opération en meme temps 
 // Afficher 101
+var a = 100;        //  $a vaut 100
+console.log(a++);   // Affiche "$a" 100, puis ajoute 1 à "$a"; à la fin de cette ligne "$a" vaut 101
+console.log(a--);   // Affiche "$a" 101, puis soustrait 1 à "$a";à la fin de cette ligne "$a" vaut 100
+console.log(++a);
+console.log(a);
+
+
+
+
+// Opérateurs de chaine
+// --
+
+// Symbole:     +
+// Syntaxe:     a + b
+// /!\ "a" et "b" sont des chaines de caractères
+var a = "Hello ";
+var b = "World";
+console.log( a + b );
+
+var firstname = "Bruce";
+console.log( "son nom est "+ firstname );
