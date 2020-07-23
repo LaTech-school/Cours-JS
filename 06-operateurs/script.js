f26fb3aa6c710165c4dca08c89113027fd2736d3
Arnaud BODEL <contact@osw3.net>
2020-07-23 11:25:46 +0200

    -

--- 06-operateurs/script.js
+++ 06-operateurs/script.js
@@ -1,1 +1,36 @@
 // Les Opérateurs
+
+// Les opérateurs d'affectation
+// --
+
+// Affectation simple 
+// Symbole:     =
+// Syntaxe:     a = b 
+var a = 42;
+
+
+
+// Les opérateurs de comparaison
+// --
+
+// Egalité
+// Symbole:     ==
+// Syntaxe:     a == b
+console.log( '42 == "42"', 42 == "42" );
+
+// Inegalite
+// Symbole:     !=
+// Syntaxe:     a != b
+console.log( '42 != "42"', 42 != "42" );
+console.log( '40 != "42"', 40 != "42" );
+console.log( '40 != 42', 40 != 42 );
+
+// Egalité strict
+// Symbole:     ===
+// Syntaxe:     a === b
+console.log( '42 === "42"', 42 === "42" );
+
+// Inégalité strict
+// Symbole:     !==
+// Syntaxe:     a !== b
+console.log( '42 !== "42"', 42 !== "42" );
