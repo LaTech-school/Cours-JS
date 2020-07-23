93f972d5d9743aa9b27f9baa0dce68219958c639
Arnaud BODEL <contact@osw3.net>
2020-07-23 10:59:56 +0200

    -

--- 04-variables/script.js
+++ 04-variables/script.js
@@ -4,6 +4,30 @@
 // -> prend la taille de ce qu'il contient
 // -> sera de type de ce qu'il contient
 
+// Syntaxe d'un nom de variable
+// -> ☑ Caractères alphabétique et caractères numérique
+// -> ☑ Majuscules / Minuscules
+// -> ☑ Tiret du 8 (underscore en anglais) "_"
+// -> ☒ Pas de caractères spéciaux (+:/;.#@&é"'(§è!çà)° ...)
+// -> ☒ Ne commence pas par un numérique
+// -> ☒ Tiret du 6 (dash en anglais) "-"
+var azertyu12345;
+var AzerTY;
+var azerty;
+var _1azerty;
+var a_b;
+var azertyu________;
+
+// Ecritue slug / snake
+var user_name;
+
+// Lower Camel Case
+var userName;
+
+// Upper Camel Case
+var UserName;
+
+
 // On déclare une variable avec le mot clé "var"
 // Suivi d'un nom de variable (ex: la variable "name")
 var name;
