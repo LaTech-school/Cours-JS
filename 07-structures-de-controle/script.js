76f4cd38c3de27082536d035f323e0b7aed8fe95
Arnaud BODEL <contact@osw3.net>
2020-07-23 13:21:54 +0200

    -

--- 07-structures-de-controle/script.js
+++ 07-structures-de-controle/script.js
@@ -4,7 +4,7 @@
 // --
 // Syntaxe:     if (a) { ... }
 
-var a = 3;
+var a = 5;
 
 // if (a > 5) {
 //     alert('Variable "a" supérieure a 5');
@@ -13,10 +13,34 @@ var a = 3;
 //     alert('Variable "a" supérieure a 15');
 // }
 
-// if (false) {
-//     alert("Vraie !");
+// if (!true) {
+//     alert("Oui c'est faux !");
 // }
 
 
+console.log( true );
+console.log( false );
+console.log( !true );
+console.log( !false );
+console.log( !0 );
+console.log( !(!0) == false );
+
 // Afficher un message si a est supérieur ou égale à 10
 // Afficher un autre message si a est inférieur à 10
+// if (a >= 10) {
+//     alert("Supérieur ou égale à 10");
+// }
+// if (a < 10) {
+//     alert("Inférieur à 10");
+// }
+
+// Condition "if..else" (si...sinon)
+// --
+// Syntaxe:     if (a) { ... } else { ... }
+// if (a >= 10) {
+//     alert("Supérieur ou égale à 10");
+// }
+// else // if (a < 10) 
+// {
+//     alert("Inférieur à 10");
+// }
