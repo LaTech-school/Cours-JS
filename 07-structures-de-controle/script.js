be5bfcf18acc42ab9a51bf86ef06a05874839035
Arnaud BODEL <contact@osw3.net>
2020-07-23 14:57:39 +0200

    -

--- 07-structures-de-controle/script.js
+++ 07-structures-de-controle/script.js
@@ -4,7 +4,7 @@
 // --
 // Syntaxe:     if (a) { ... }
 
-var a = 5;
+// var a = 5;
 
 // if (a > 5) {
 //     alert('Variable "a" supérieure a 5');
@@ -13,20 +13,19 @@ var a = 5;
 //     alert('Variable "a" supérieure a 15');
 // }
 
-// if (!true) {
-//     alert("Oui c'est faux !");
-// }
+// // if (!true) {
+// //     alert("Oui c'est faux !");
+// // }
 
+// // console.log( true );
+// // console.log( false );
+// // console.log( !true );
+// // console.log( !false );
+// // console.log( !0 );
+// // console.log( !(!0) == false );
 
-console.log( true );
-console.log( false );
-console.log( !true );
-console.log( !false );
-console.log( !0 );
-console.log( !(!0) == false );
-
-// Afficher un message si a est supérieur ou égale à 10
-// Afficher un autre message si a est inférieur à 10
+// // Afficher un message si a est supérieur ou égale à 10
+// // Afficher un autre message si a est inférieur à 10
 // if (a >= 10) {
 //     alert("Supérieur ou égale à 10");
 // }
@@ -34,13 +33,61 @@ console.log( !(!0) == false );
 //     alert("Inférieur à 10");
 // }
 
-// Condition "if..else" (si...sinon)
-// --
-// Syntaxe:     if (a) { ... } else { ... }
+// // Condition "if..else" (si...sinon)
+// // --
+// // Syntaxe:     if (a) { ... } else { ... }
 // if (a >= 10) {
 //     alert("Supérieur ou égale à 10");
 // }
 // else // if (a < 10) 
 // {
 //     alert("Inférieur à 10");
+// }
+
+
+
+// var age = 15;
+
+// if (age >= 18) {
+//     // adulte
+// }
+// if (age < 13) {
+//     // Enfant
+// }
+
+// if (age >= 18) {
+//     // adulte
+// }
+// else {
+//     // pas adulte
+// }
+
+
+
+
+
+// var age = 10;
+
+// if (age >= 18) {
+//     console.log("Adulte");
+// }
+// if (age < 18) {
+//     console.log("Ado");
+// }
+// if (age < 13) {
+//     console.log("Enfant");
+// }
+
+
+// Condition "if..else if ... else" (si...sinon si .... sinon)
+
+// var age = 15;
+// if (age >= 18) {
+//     console.log("Adulte");
+// } 
+// else if (age < 13) {
+//     console.log("Enfant");
+// }
+// else {
+//     console.log("Ado");
 // }
