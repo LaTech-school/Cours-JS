89fd3bde0eb37845c396915f2a3458afed6eb7b8
Arnaud BODEL <contact@osw3.net>
2020-07-23 17:34:20 +0200

    -

---
+++ 08-boucles/script.js
@@ -0,0 +1,21 @@
+// Les Boucles
+// --
+// Pourquoi ? 
+// -> pour executer du code de façon réptitif !
+// -> pour parcourir un tableau ou un objet
+
+// Boucle : While
+// Execute du code tant que la condition est vérifiée
+
+var i = 4;
+
+// while (i < 10) {
+//     console.log("i vaux "+ ++i);
+// }
+
+
+// Boucle : Do While
+// Execute du code au moins une fois le code avant de verifier la condition
+do {
+    console.log("i vaux "+ i++);
+} while (i < 10)
--- TP/tp-02/script.js
+++ TP/tp-02/script.js
@@ -4,12 +4,30 @@
 // Recupérer la réponse
 var start = confirm("Bienvenu, voulez vous commencer ?");
 
-console.log(start);
-
 // Si l'utilisateur dit "OK", on lui demande son prénom via une boite de dialogue prompt
-// Recupérer la réponse et dire "Bonjour $user" via une boite de dialogue alert
+if (start) {
 
-// On demande l'age de l'utilisateur
-// Recupérer la réponse
+    var name = prompt("Quel est ton nom ?");
+    // Recupérer la réponse et dire "Bonjour $user" via une boite de dialogue alert
+    if (name != "null") {
+        alert("Bounjour "+name);
 
-// Si l'utilisateur a plus de 12 ans, il sera autorisé a acceder au site sinon on lui affiche un message "Tu es trop jeune, reviens plus tard !"
+        // On demande l'age de l'utilisateur
+        // Recupérer la réponse
+        var age = prompt("Quel est ton age ?");
+
+        // Si l'utilisateur a plus de 12 ans, il sera autorisé a acceder au site sinon on lui affiche un message "Tu es trop jeune, reviens plus tard !"
+        if (age > 12) {
+            alert("Welcome "+name);
+        } else {
+            alert("Tu es trop jeune, reviens plus tard !");
+        }
+    }
+    else {
+        alert("Ooops je n'ai pas compris ton nom !");
+    }
+    
+}
+else {
+    alert("Ok... Bye !");
+}
