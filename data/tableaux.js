b8f79c432bb0effc3257dbdfc4426feb0137cc70
Arnaud BODEL <contact@osw3.net>
2020-07-24 14:36:32 +0200

    -

---
+++ data/tableaux.js
@@ -0,0 +1,24 @@
+// Tableau de chaines
+var fruits = [
+    "Pommes", 
+    "Poires", 
+    "Bananes"
+];
+
+// Tableau d'objets
+var users = [
+    // User 0
+    {
+        firstname: "Bruce",
+        lastname: "Wayne",
+    },
+
+    // User 1
+    {
+        firstname: "Clark",
+        lastname: "Kent",
+    }
+];
+
+// Afficher Bruce
+console.log( users[0].firstname );
