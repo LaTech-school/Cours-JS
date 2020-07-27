26b01ef2a078e4c01f6b73e4090ab8697f5c9ce0
Arnaud BODEL <contact@osw3.net>
2020-07-27 11:07:37 +0200

    -

--- 11-DOM/script.js
+++ 11-DOM/script.js
@@ -20,20 +20,35 @@
 var theTitle = "Document Object Model";
 document.title = theTitle;
 
-// Ciblage d'un element HTML par sont ID
+// Ciblage d'un element HTML par son ID
 var mainHeader = document.getElementById('main-header');
 
 // Debug du type du contenu du noeud HTML
-console.log(typeof mainHeader);
+// console.log(typeof mainHeader);
 
 // Debug du contenu du noeud HTML
-console.log(mainHeader);
+// console.log(mainHeader);
 
 // Debug du contenu HTML du noeud HTML
-console.log(mainHeader.innerHTML);
+// console.log(mainHeader.innerHTML);
 
 // Modification du contenu du noeud HTML
 mainHeader.innerHTML = "<strong>Hello</strong> World";
 
 // Debug du contenu HTML du noeud HTML
-console.log(mainHeader.innerHTML);
+// console.log(mainHeader.innerHTML);
+
+
+// Ciblage d'elements HTML par nom de class
+var mainTitle = document.getElementsByClassName('main-title');
+
+// /!\ mainTitle est une liste, un tableau de noeuds HTML
+// mainTitle doit être parcouru comme un tableau
+
+var mainHeader = document.getElementById('main-header');
+
+// Debug de la liste des noeuds HTML
+// console.log(mainHeader);
+
+// Debug du contenu HTML du noeud à l'index O du tableau mainTitle
+console.log(mainTitle[0]);
