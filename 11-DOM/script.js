b3d24908b9b2b254dc8dcd2550f1ba7f5b82930e
Arnaud BODEL <contact@osw3.net>
2020-07-27 10:51:03 +0200

    -

--- 11-DOM/script.js
+++ 11-DOM/script.js
@@ -16,3 +16,24 @@
 //          |   |   |   |   |-- P
 //          |   |-- ASIDE
 //          |-- FOOTER
+
+var theTitle = "Document Object Model";
+document.title = theTitle;
+
+// Ciblage d'un element HTML par sont ID
+var mainHeader = document.getElementById('main-header');
+
+// Debug du type du contenu du noeud HTML
+console.log(typeof mainHeader);
+
+// Debug du contenu du noeud HTML
+console.log(mainHeader);
+
+// Debug du contenu HTML du noeud HTML
+console.log(mainHeader.innerHTML);
+
+// Modification du contenu du noeud HTML
+mainHeader.innerHTML = "<strong>Hello</strong> World";
+
+// Debug du contenu HTML du noeud HTML
+console.log(mainHeader.innerHTML);
