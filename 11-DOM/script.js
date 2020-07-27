1bd1f0f1a181df24408f3d0f6a526c45fb5ad24a
Arnaud BODEL <contact@osw3.net>
2020-07-27 13:07:58 +0200

    -

--- 11-DOM/script.js
+++ 11-DOM/script.js
@@ -51,4 +51,71 @@ var mainHeader = document.getElementById('main-header');
 // console.log(mainHeader);
 
 // Debug du contenu HTML du noeud à l'index O du tableau mainTitle
-console.log(mainTitle[0]);
+// console.log(mainTitle[0]);
+
+// Modification du contenu HTML de la premiere classe .main-title
+var mainTitle = document.getElementsByClassName('main-title');
+// console.log(typeof mainTitle);
+// console.log(mainTitle);
+// console.log(mainTitle[0]);
+
+// Condition pour executer le innerHTML si mainTitle[0] n'est pas undefined
+if (mainTitle[0] != undefined)
+{
+    mainTitle[0].innerHTML = "C'est la pause !!!";
+    mainTitle[0].style = "color: red";
+}
+
+var allDivs = document.getElementsByTagName('DIV');
+// console.log(allDivs);
+
+var allParagraphes = document.getElementsByTagName('P');
+console.log(allParagraphes);
+
+for (let i=0; i < allParagraphes.length; i++) 
+{
+    console.log( allParagraphes[i] );
+    allParagraphes[i].style = "border: 10px dotted red";
+}
+
+
+
+
+
+// Résumé
+
+// Ciblage d'un element HTML par son ID
+// <div id="main-header">
+
+// Retourne un noeud HTML 
+// ex: <div id="main-header">...</div>
+document.getElementById('main-header');
+
+
+
+// Ciblage d'elements HTML par nom de class
+// <div class="main-title">
+
+// Retourne une liste de noeuds HTML 
+// ex : array(
+//      "<div class=main-title>...</div>", 
+//      "<h1 class=main-title>...</h1>"
+// )
+document.getElementsByClassName('main-title');
+
+
+
+
+
+// Ciblage d'elements HTML par nom de balise / Tag
+// <div>
+
+// Retourne une liste de noeuds HTML 
+// ex : array(
+//      "<div>...</div>", 
+//      "<div>...</div>"
+// )
+document.getElementsByTagName('DIV');
+// <section>
+document.getElementsByTagName('SECTION');
+
