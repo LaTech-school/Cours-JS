fbf6c2400a748c32a13aa78f9271b88111b9cbc1
Arnaud BODEL <contact@osw3.net>
2020-07-27 10:21:11 +0200

    -

---
+++ 11-DOM/script.js
@@ -0,0 +1,17 @@
+// Document Object Model
+
+// Root
+// |--- HTML
+//      |-- HEAD
+//      |   |-- TITLE
+//      |-- BODY
+//          |-- HEADER
+//          |-- DIV.main-content
+//          |   |-- MAIN
+//          |   |   |-- SECTION
+//          |   |   |   |-- ARTICLE
+//          |   |   |   |   |-- H1
+//          |   |   |   |   |-- P
+//          |   |   |   |   |-- P
+//          |   |-- ASIDE
+//          |-- FOOTER
