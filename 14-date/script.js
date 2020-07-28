3f77efc063ba51d6a2fb9bc5af746b8174bd1498
Arnaud BODEL <contact@osw3.net>
2020-07-28 12:30:55 +0200

    -

---
+++ 14-date/script.js
@@ -0,0 +1,35 @@
+// Instance de l'objet Date basé sur l'horloge du client
+var date = new Date();
+// console.log(date);
+
+// recup. de l'année courante
+var year = date.getFullYear();
+console.log(year);
+
+// recup. du mois courant
+var month = date.getMonth();
+console.log(month);
+
+// recup. du numero de jour de la semaine
+var weekDay = date.getDay();
+console.log(weekDay);
+
+// recup. du jour courant
+var day = date.getDate();
+console.log(day);
+
+// recup. de l'heure
+var hour = date.getHours();
+console.log(hour);
+
+// recup. des minute
+var minute = date.getMinutes();
+console.log(minute);
+
+// recup. des secondes
+var second = date.getSeconds();
+console.log(second);
+
+// recup. des millisecondes
+var ms = date.getMilliseconds();
+console.log(ms);
