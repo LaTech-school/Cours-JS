ff3ba9cac4abc28bfdd6709eafb537765c077252
Arnaud BODEL <contact@osw3.net>
2020-07-28 12:19:58 +0200

    -

---
+++ 13-controle-de-formulaire/script.js
@@ -0,0 +1,58 @@
+"use strict";
+
+// Générer les champs "Date of Birth"
+// --
+
+var birthday = document.getElementById('birth_day');
+var birthmonth = document.getElementById('birth_month');
+var birthyear = document.getElementById('birth_year');
+
+birthday.style = "border: 10px solid blue";
+birthmonth.style = "border: 10px solid yellow";
+birthyear.style = "border: 10px solid red";
+
+// Générer le champ "birthday"
+for (let i=1; i<=31; i++) 
+{
+    // Create d'une balise HTML
+    let option = document.createElement('OPTION');
+
+    // Ajoute de l'attribut "value" sur l'<option>
+    option.setAttribute('value', i);
+
+    // Injection du texte dans l'<option> avec un zero qui précède l'unité
+    option.textContent = (i <= 9) ? "0"+i : i;
+
+    // Inject <option> dans le <select>
+    birthday.appendChild(option);
+}
+
+
+// Générer le champ "birthmonth"
+
+
+// Générer le champ "birthyear"
+
+
+
+
+
+
+
+
+// Controle du formulaire
+// --
+
+// Ciblage du <form> + stockage dans la variable "form"
+var form = document.getElementsByTagName('FORM')[0];
+
+// Ecoute l'evenement de soumission du formulaire
+form.addEventListener('submit', event => {
+
+    console.log("Envois du form");
+
+    // Annule la soumission du formulaire
+    // preventDefault > annule le comportement par defaut de l'evenement "submit"
+    event.preventDefault();
+    // equi: <form onsubmit="return false;">
+});
